690115045859d8fcc6ceeccf0b3f6ae302b95f5e
Christian Hammond <christian@beanbaginc.com>
2022-12-05 03:28:49 -0800

    Update Day 5 for more complexity in parsing.
    I didn't like that I hard-coded the initial stacks, so I've updated the
    parsing to have a two-phase approach. During setup, I parse stack data,
    without assumptions on number of stacks. Once the number row is
    detected, parsing moves to the moves as before.

--- day5/task1.py
+++ day5/task1.py
@@ -1,38 +1,56 @@
 #!/usr/bin/env python3
+#
+# NOTE: I originally hard-coded the crates, but that felt too easy. So now
+#       I parse them from the input. And I don't assume number of stacks.
+#       Why not.
 
 import re
 
 
-stacks = [
-    ['T', 'D', 'W', 'Z', 'V', 'P'],
-    ['L', 'S', 'W', 'V', 'F', 'J', 'D'],
-    ['Z', 'M', 'L', 'S', 'V', 'T', 'B', 'H'],
-    ['R', 'S', 'J'],
-    ['C', 'Z', 'B', 'G', 'F', 'M', 'L', 'W'],
-    ['Q', 'W', 'V', 'H', 'Z', 'R', 'G', 'B'],
-    ['V', 'J', 'P', 'C', 'B', 'D', 'N'],
-    ['P', 'T', 'B', 'Q'],
-    ['H', 'G', 'Z', 'R', 'C'],
-]
-
-
 MOVE_RE = re.compile(r'^move (\d+) from (\d+) to (\d+)')
+CRATES_RE = re.compile(r'(?:\[(?P<cargo>[A-Z])\]|   ) ?')
+
+stacks = []
+in_setup = True
 
 
 with open('input', 'r') as fp:
     for line in fp.readlines():
-        m = MOVE_RE.match(line)
-        assert m
+        if in_setup:
+            # Load in crate data.
+            if line.lstrip().startswith('1 '):
+                # We've reached the numbers row. We're done with setup.
+                in_setup = False
+            else:
+                cargo_items = CRATES_RE.findall(line)
 
-        move_count = int(m.group(1))
-        move_from = int(m.group(2)) - 1
-        move_to = int(m.group(3)) - 1
+                if not stacks:
+                    stacks = [
+                        []
+                        for i in cargo_items
+                    ]
 
-        stacks[move_to] += reversed(stacks[move_from][-move_count:])
-        stacks[move_from] = stacks[move_from][:-move_count]
+                for i, cargo in enumerate(cargo_items):
+                    if cargo:
+                        stacks[i].insert(0, cargo)
+        else:
+            # Process the moves.
+            line = line.strip()
+
+            if line:
+                # Process the moves.
+                m = MOVE_RE.match(line)
+                assert m
+
+                move_count = int(m.group(1))
+                move_from = int(m.group(2)) - 1
+                move_to = int(m.group(3)) - 1
+
+                stacks[move_to] += reversed(stacks[move_from][-move_count:])
+                stacks[move_from] = stacks[move_from][:-move_count]
 
 
 print(''.join(
-    crate[-1]
-    for crate in stacks
+    stack[-1]
+    for stack in stacks
 ))
