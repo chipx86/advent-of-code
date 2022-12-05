49cf1e1a75c723e47571c5b40abb5630027e3246
Christian Hammond <christian@beanbaginc.com>
2022-12-05 02:39:21 -0800

    Add my solutions for day 5.

---
+++ day5/task1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import re
+
+
+stacks = [
+    ['T', 'D', 'W', 'Z', 'V', 'P'],
+    ['L', 'S', 'W', 'V', 'F', 'J', 'D'],
+    ['Z', 'M', 'L', 'S', 'V', 'T', 'B', 'H'],
+    ['R', 'S', 'J'],
+    ['C', 'Z', 'B', 'G', 'F', 'M', 'L', 'W'],
+    ['Q', 'W', 'V', 'H', 'Z', 'R', 'G', 'B'],
+    ['V', 'J', 'P', 'C', 'B', 'D', 'N'],
+    ['P', 'T', 'B', 'Q'],
+    ['H', 'G', 'Z', 'R', 'C'],
+]
+
+
+MOVE_RE = re.compile(r'^move (\d+) from (\d+) to (\d+)')
+
+
+with open('input', 'r') as fp:
+    for line in fp.readlines():
+        m = MOVE_RE.match(line)
+        assert m
+
+        move_count = int(m.group(1))
+        move_from = int(m.group(2)) - 1
+        move_to = int(m.group(3)) - 1
+
+        stacks[move_to] += reversed(stacks[move_from][-move_count:])
+        stacks[move_from] = stacks[move_from][:-move_count]
+
+
+print(''.join(
+    crate[-1]
+    for crate in stacks
+))
