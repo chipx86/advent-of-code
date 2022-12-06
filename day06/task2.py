a4f216748ff102c9b5996b536d9a748f4fe158c6
Christian Hammond <christian@beanbaginc.com>
2022-12-05 21:19:02 -0800

    Add my solution for day 6.

---
+++ day06/task2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+MARKER_LEN = 14
+
+
+with open('input', 'r') as fp:
+    count = 0
+    buf = []
+
+    while len(set(buf)) != MARKER_LEN:
+        count += 1
+        c = fp.read(1)
+
+        if not c:
+            break
+
+        buf = buf[-(MARKER_LEN - 1):] + [c]
+
+
+print('code = %r' % ''.join(buf))
+print('count = %s' % count)
